f7a7808b373f4b311c71a172c1905d463fbf975c
yoshi-automation <yoshi-automation@google.com>
2019-06-05 07:23:25 -0700

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- src/v1/subscriber_client.js
+++ src/v1/subscriber_client.js
@@ -57,14 +57,18 @@ class SubscriberClient {
    *     API remote host.
    */
   constructor(opts) {
+    opts = opts || {};
     this._descriptors = {};
+
+    const servicePath =
+      opts.servicePath || opts.apiEndpoint || this.constructor.servicePath;
 
     // Ensure that options include the service address and port.
     opts = Object.assign(
       {
         clientConfig: {},
         port: this.constructor.port,
-        servicePath: this.constructor.servicePath,
+        servicePath,
       },
       opts
     );
@@ -239,6 +243,25 @@ class SubscriberClient {
    * The DNS address for this API service.
    */
   static get servicePath() {
+    return 'pubsub.googleapis.com';
+
+    // note: editing generated code
+    this.waitForReady = function(deadline, callback) {
+      return subscriberStub.then(
+        stub => stub.waitForReady(deadline, callback),
+        callback
+      );
+    };
+    this.getSubscriberStub = function() {
+      return subscriberStub;
+    };
+  }
+
+  /**
+   * The DNS address for this API service - same as servicePath(),
+   * exists for compatibility reasons.
+   */
+  static get apiEndpoint() {
     return 'pubsub.googleapis.com';
   }
 
