9e8f245e982cb0422d39bdfc042194ac43994a96
Dave Gramlich <callmehiphop@gmail.com>
2019-06-07 15:20:03 -0700

    fix: update regex to target correct comment (#646)

--- src/message-queues.ts
+++ src/message-queues.ts
@@ -52,9 +52,7 @@ export class BatchError extends Error implements ServiceError {
   metadata?: Metadata;
   constructor(err: ServiceError, ackIds: string[], rpc: string) {
     super(
-      `Failed to "${rpc}" for ${ackIds.length} message(s). Reason: ${
-        err.message
-      }`
+      `Failed to "${rpc}" for ${ackIds.length} message(s). Reason: ${err.message}`
     );
 
     this.ackIds = ackIds;
