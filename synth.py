a1360f2ad143dc9600a1d09e943a4bfa0ffd0016
summerji <summerji@google.com>
2020-03-26 23:53:25 -0700

    hack synth.py to fix duplicate http namespace

--- synth.py
+++ synth.py
@@ -2,6 +2,7 @@ import synthtool as s
 import synthtool.gcp as gcp
 import logging
 import subprocess
+import json
 import os
 
 logging.basicConfig(level=logging.DEBUG)
@@ -24,8 +25,8 @@ library = gapic.typescript_library(
         'template': f'typescript_gapic'
     },
     proto_path=f'/google/pubsub/{version}',
-    extra_proto_files=['google/iam/v1/.',
-                       'google/cloud/common_resources.proto']
+    extra_proto_files=['google/iam/v1/', 'google/api/',
+                       'google/type']
 )
 
 # skip index, protos, package.json, and README.md
@@ -35,6 +36,22 @@ s.copy(
 
 templates = common_templates.node_library(source_location='build/src')
 s.copy(templates)
+
+# Remove unneeded protos in proto_list.json
+# In micro-generators, we do have default multiple common protos set as dependencies.
+# But if the proto is also specified in protoc command (used to generating client libraries),
+# protoc command will fail becasue of the repetition and force us to remove the duplicate protos reference
+list_jsons = ['src/v1/publisher_proto_list.json',
+              'src/v1/subscriber_proto_list.json']
+remove_proto_keywords = ['/google/api', '/google/protobuf', '/google/type']
+for list_json in list_jsons:
+    with open(list_json, 'r') as f:
+        items = json.load(f)
+        content = [item for item in items if all(
+            [(x not in item) for x in remove_proto_keywords])]
+        new_file = json.dumps(content, indent=2) + '\n'
+    with open(list_json, 'w') as f:
+        f.write(new_file.replace('logging/audit_data.proto', 'iam_policy.proto'))
 
 # fix tslint issue due to mismatch gts version with gapic-generator-typescript
 # it should be removed once pubsub upgrade gts 2.0.0
