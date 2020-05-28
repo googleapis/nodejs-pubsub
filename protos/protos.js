65efe339f52cd75462bad407e0a5fadbd19f3f85
yoshi-automation <yoshi-automation@google.com>
2020-05-28 05:39:04 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- protos/protos.js
+++ protos/protos.js
@@ -28,7 +28,7 @@
     var $Reader = $protobuf.Reader, $Writer = $protobuf.Writer, $util = $protobuf.util;
     
     // Exported root namespace
-    var $root = $protobuf.roots._google_cloud_pubsub_1_7_1_protos || ($protobuf.roots._google_cloud_pubsub_1_7_1_protos = {});
+    var $root = $protobuf.roots._google_cloud_pubsub_2_0_0_protos || ($protobuf.roots._google_cloud_pubsub_2_0_0_protos = {});
     
     $root.google = (function() {
     
