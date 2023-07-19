3c5a34b9700d3f6cfbb59127b6fd9fe919033a03
feywind <57276408+feywind@users.noreply.github.com>
2023-07-19 15:24:24 -0400

    feat!: gax 4 updates - some of this will be regen'd, just testing

--- src/v1/subscriber_client.ts
+++ src/v1/subscriber_client.ts
@@ -2159,7 +2159,7 @@ export class SubscriberClient {
       IamProtos.google.iam.v1.GetIamPolicyRequest | null | undefined,
       {} | null | undefined
     >
-  ): Promise<IamProtos.google.iam.v1.Policy> {
+  ): Promise<[IamProtos.google.iam.v1.Policy]> {
     return this.iamClient.getIamPolicy(request, options, callback);
   }
 
@@ -2207,7 +2207,7 @@ export class SubscriberClient {
       IamProtos.google.iam.v1.SetIamPolicyRequest | null | undefined,
       {} | null | undefined
     >
-  ): Promise<IamProtos.google.iam.v1.Policy> {
+  ): Promise<[IamProtos.google.iam.v1.Policy]> {
     return this.iamClient.setIamPolicy(request, options, callback);
   }
 
