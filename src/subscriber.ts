9a2ca06bb7641d502624917401f54da0ce02cdb2
Luis Gustavo <luisgustavomacedo13@gmail.com>
2025-07-19 12:46:29 -0300

    fix: remove message from inventory when nack with response

--- src/subscriber.ts
+++ src/subscriber.ts
@@ -887,6 +887,8 @@ export class Subscriber extends EventEmitter {
     const latency = (Date.now() - startTime) / 1000;
     this._latencies.add(latency);
 
+    this._inventory.remove(message);
+
     // No exception means Success.
     return AckResponses.Success;
   }
